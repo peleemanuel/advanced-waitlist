1b0d8992c3138a0b1575036c03b428d68287b6db
George Pop <george.pop@student.upt.ro>
2026-05-07 23:48:08 +0300

    created docker files to build everything with 1 command

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -5,7 +5,13 @@ import { OpenFeature } from "@openfeature/server-sdk";
 import { FlagdProvider } from "@openfeature/flagd-provider";
 
 async function bootstrap() {
-  await OpenFeature.setProviderAndWait(new FlagdProvider());
+  await OpenFeature.setProviderAndWait(
+    new FlagdProvider({
+      host: process.env.FLAGD_HOST ?? "localhost",
+      port: Number(process.env.FLAGD_PORT ?? 8013),
+    }),
+  );
+
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
